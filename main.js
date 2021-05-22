62e8c0760187f76f60b691f478f9d9c3d7514c06
hiarif21 <hiarif21@gmail.com>
2021-05-22 20:41:38 +0700

    update

--- main.js
+++ main.js
@@ -4,34 +4,11 @@ const sheet_name_list = workbook.SheetNames;
 // get a list sheetnames
 // console.log(sheet_name_list);
 
-sheet_name_list.forEach(function (y) {
-  let worksheet = workbook.Sheets[y];
+sheet_name_list.forEach(function (item) {
+  let worksheet = workbook.Sheets[item];
   // get detail sheet
   // console.log(worksheet);
 
-  let headers = {};
-  let data = [];
-  for (cell in worksheet) {
-    if (cell[0] === "!") continue;
-    // get cell
-    // console.log(cell);
-
-    // parse col, row and value
-    let col = cell.substring(0, 1);
-    let row = parseInt(cell.substring(1));
-    let value = worksheet[cell].v;
-
-    // store header name
-    if (row == 1) {
-      headers[col] = value;
-      continue;
-    }
-
-    if (!data[row]) data[row] = {};
-    data[row][headers[col]] = value;
-  }
-
-  data.shift();
-  data.shift();
+  let data = XLSX.utils.sheet_to_json(worksheet);
   console.log(data);
 });
